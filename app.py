543bb9372e375e9e6a5548775af33f7c2c85ca1c
Gabriela Quinde <50193708+GabbyLiz@users.noreply.github.com>
2024-03-01 17:12:15 -0500

    Update app.py
    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import gdown
 from tensorflow.keras.models import load_model
 import os
 
+st.title('Mi Aplicación Streamlit con Modelo Preentrenado')
 # Enlace compartido de Google Drive al archivo HDF5 (reemplaza 'your_file_id')
 enlace_google_drive = 'https://drive.google.com/uc?id=1uiJR1cD2W1cNVpqG77Th6XHhWSxuEVwW'
 
@@ -12,3 +13,18 @@ gdown.download(enlace_google_drive, nombre_archivo_local)
 st.success('Modelo HDF5 descargado correctamente.')
 ruta_archivo = "modelo.hdf5"
 st.write("Ruta completa del archivo:", os.path.abspath(ruta_archivo))
+
+ruta_archivo = "/mount/src/prueba/modelo.hdf5"
+# Cargar el modelo desde el archivo HDF5
+modelo_cargado = load_model(ruta_archivo)
+
+# Mostrar información sobre el modelo
+
+st.write('Información sobre el modelo:')
+st.write(modelo_cargado.summary())
+
+# Realizar predicciones, etc., según sea necesario con el modelo cargado
+# ...
+
+# Eliminar el archivo HDF5 después de su uso (opcional)
+os.remove(nombre_archivo_local)

ccdfbc752e412921ae1ba853ae05e89fcba77825
Gabriela Quinde <50193708+GabbyLiz@users.noreply.github.com>
2024-03-01 18:18:17 -0500

    Update app.py
    Update app.py

--- app.py
+++ app.py
@@ -33,9 +33,10 @@ st.write("Ruta completa del archivo:", ruta_archivo)
 try:
     st.info('Cargando el modelo...')
     with h5py.File(ruta_archivo, 'r') as file:
+        st.success('Modelo cargado exitosamente.')
         # Aquí, puedes acceder a las capas y demás información del modelo si es necesario
         # Por ejemplo: capa_input = file['input_layer_name']
         # Pero, por ahora, solo mostraremos un mensaje de éxito
-    st.success('Modelo cargado exitosamente.')
+    
 except Exception as e:
     st.error(f'Error al cargar el modelo: {str(e)}')

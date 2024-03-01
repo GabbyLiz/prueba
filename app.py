f7f8dc0728c721b8b79447f02a27bfec91670a78
Gabriela Quinde <50193708+GabbyLiz@users.noreply.github.com>
2024-03-01 18:36:30 -0500

    Update app.py
    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
 import streamlit as st
 from tensorflow.keras.models import load_model
 import gdown
-import os
 
 st.title('Mi Aplicación Streamlit con Modelo Preentrenado')
 
 # Enlace compartido de Google Drive al archivo HDF5 (reemplaza 'your_file_id')
 enlace_google_drive = 'https://drive.google.com/uc?id=1uiJR1cD2W1cNVpqG77Th6XHhWSxuEVwW'
 
-# Nombre del archivo HDF5 local
-nombre_archivo_local = 'modelo.hdf5'
-
 # Descargar el modelo desde Google Drive en tiempo real
-gdown.download(enlace_google_drive, nombre_archivo_local, quiet=False)
-
-# Cargar el modelo
-modelo_cargado = load_model(nombre_archivo_local)
+modelo_cargado = load_model(gdown.cached_download(enlace_google_drive))
 
 # Hacer predicciones o cualquier otra cosa con el modelo cargado
 # ...

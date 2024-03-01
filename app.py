e508c12299dc72e089efbf691bb358f607459817
Gabriela Quinde <50193708+GabbyLiz@users.noreply.github.com>
2024-03-01 17:19:34 -0500

    Update app.py
    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,22 @@ from tensorflow.keras.models import load_model
 import os
 
 st.title('Mi Aplicación Streamlit con Modelo Preentrenado')
-# Enlace compartido de Google Drive al archivo HDF5 (reemplaza 'your_file_id')
 enlace_google_drive = 'https://drive.google.com/uc?id=1uiJR1cD2W1cNVpqG77Th6XHhWSxuEVwW'
-
-# Nombre del archivo HDF5 local
 nombre_archivo_local = 'modelo.hdf5'
-gdown.download(enlace_google_drive, nombre_archivo_local)
-st.success('Modelo HDF5 descargado correctamente.')
+
+# Descargar el modelo solo si no está presente
+if not os.path.exists(nombre_archivo_local):
+    gdown.download(enlace_google_drive, nombre_archivo_local)
+    st.success('Modelo HDF5 descargado correctamente.')
+
 ruta_archivo = "modelo.hdf5"
-st.write("Ruta completa del archivo:", os.path.abspath(ruta_archivo))
+ruta_completa = os.path.join(os.getcwd(), ruta_archivo)
+
+st.write("Ruta completa del archivo:", ruta_completa)
+
+# Intentar cargar el modelo
+try:
+    modelo_cargado = load_model(ruta_completa)
+    st.success('Modelo cargado exitosamente.')
+except Exception as e:
+    st.error(f'Error al cargar el modelo: {str(e)}')

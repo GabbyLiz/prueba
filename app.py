31ab10957dc79a532a39a1c1e9e91062e90f6ac4
Gabriela Quinde <50193708+GabbyLiz@users.noreply.github.com>
2024-03-01 16:53:30 -0500

    Update app.py
    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from tensorflow.keras.models import load_model
 import os
 
 # Enlace compartido de Google Drive al archivo HDF5 (reemplaza 'your_file_id')
-enlace_google_drive = 'https://drive.google.com/uc?id=your_file_id'
+enlace_google_drive = 'https://drive.google.com/uc?id=1uiJR1cD2W1cNVpqG77Th6XHhWSxuEVwW'
 
 # Nombre del archivo HDF5 local
 nombre_archivo_local = 'modelo.hdf5'

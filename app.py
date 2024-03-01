1a68bd20c12181c3deded03cc6fcca83dbe8830d
Gabriela Quinde <50193708+GabbyLiz@users.noreply.github.com>
2024-03-01 17:07:50 -0500

    Update app.py
    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ enlace_google_drive = 'https://drive.google.com/uc?id=1uiJR1cD2W1cNVpqG77Th6XHhW
 nombre_archivo_local = 'modelo.hdf5'
 gdown.download(enlace_google_drive, nombre_archivo_local)
 st.success('Modelo HDF5 descargado correctamente.')
+ruta_archivo = "modelo.hdf5"
 st.write("Ruta completa del archivo:", os.path.abspath(ruta_archivo))

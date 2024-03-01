405d8e8b90c4696aa496ebd46ab74df5842bcbd7
Gabriela Quinde <50193708+GabbyLiz@users.noreply.github.com>
2024-03-01 17:49:52 -0500

    Update app.py
    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import gdown
-import os
-import h5py
-import numpy as np
+from tensorflow.keras.models import load_model
 
 st.title('Mi Aplicación Streamlit con Modelo Preentrenado')
 
@@ -13,7 +11,7 @@ enlace_google_drive = 'https://drive.google.com/uc?id=1uiJR1cD2W1cNVpqG77Th6XHhW
 nombre_archivo_local = 'modelo.hdf5'
 
 # Comprobar si el modelo ya está descargado
-if not os.path.exists(nombre_archivo_local):
+if not st.file_uploader("Cargar modelo", type=["h5"]):
     st.info('Descargando el modelo...')
     try:
         gdown.download(enlace_google_drive, nombre_archivo_local, quiet=False)
@@ -23,26 +21,10 @@ if not os.path.exists(nombre_archivo_local):
 else:
     st.info('El modelo ya está descargado.')
 
-# Comprobar el contenido del directorio actual
-contenido_directorio = os.listdir()
-st.write('Contenido del directorio actual:', contenido_directorio)
-
 # Intentar cargar el modelo directamente desde el archivo HDF5
 try:
-    with h5py.File(nombre_archivo_local, 'r') as file:
-        # Obtener la arquitectura, pesos y configuración del modelo
-        st.info('Cargando la arquitectura, los pesos y la configuración del modelo...')
-        model_config = file.attrs.get('model_config')
-        if model_config is None:
-            st.error('No se encontró la configuración del modelo en el archivo HDF5.')
-        else:
-            model_config = json.loads(model_config.decode('utf-8'))
-            loaded_model = model_from_config(model_config)
-
-            # Cargar los pesos del modelo
-            st.info('Cargando los pesos del modelo...')
-            loaded_model.load_weights('model_weights')
-
-            st.success('Modelo cargado exitosamente.')
+    st.info('Cargando el modelo...')
+    modelo_cargado = load_model(nombre_archivo_local)
+    st.success('Modelo cargado exitosamente.')
 except Exception as e:
     st.error(f'Error al cargar el modelo: {str(e)}')

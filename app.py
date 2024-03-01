366581069cee9370fbe6158b1d871779c7e0092f
Gabriela Quinde <50193708+GabbyLiz@users.noreply.github.com>
2024-03-01 18:44:56 -0500

    Update app.py
    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from tensorflow.keras.models import load_model
 import gdown
 import requests
 from io import BytesIO
+import tempfile
+import os
 
 st.title('Mi Aplicación Streamlit con Modelo Preentrenado')
 
@@ -11,10 +13,13 @@ enlace_google_drive = 'https://drive.google.com/uc?id=1uiJR1cD2W1cNVpqG77Th6XHhW
 
 # Función para cargar el modelo desde Google Drive
 def cargar_modelo_desde_drive(enlace):
-    with st.spinner('Cargando el modelo...'):
+    with st.spinner('Descargando el modelo...'):
         response = requests.get(enlace)
-        model_bytes = BytesIO(response.content)
-        modelo_cargado = load_model(model_bytes)
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        temp_file.write(response.content)
+        temp_file.close()
+        modelo_cargado = load_model(temp_file.name)
+        os.remove(temp_file.name)
     return modelo_cargado
 
 # Cargar el modelo en tiempo real

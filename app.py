3706afb7d88e3175f0b1022e159463ab282d7c90
Gabriela Quinde <50193708+GabbyLiz@users.noreply.github.com>
2024-03-01 17:02:21 -0500

    Actualización app.py
    Actualización app.py

--- app.py
+++ app.py
@@ -8,22 +8,4 @@ enlace_google_drive = 'https://drive.google.com/uc?id=1uiJR1cD2W1cNVpqG77Th6XHhW
 
 # Nombre del archivo HDF5 local
 nombre_archivo_local = 'modelo.hdf5'
-
-# Descargar el archivo desde Google Drive
-with st.spinner('Descargando modelo HDF5...'):
-    gdown.download(enlace_google_drive, nombre_archivo_local)
-    st.success('Modelo HDF5 descargado correctamente.')
-
-# Cargar el modelo desde el archivo HDF5
-modelo_cargado = load_model(nombre_archivo_local)
-
-# Mostrar información sobre el modelo
-st.title('Mi Aplicación Streamlit con Modelo Preentrenado')
-st.write('Información sobre el modelo:')
-st.write(modelo_cargado.summary())
-
-# Realizar predicciones, etc., según sea necesario con el modelo cargado
-# ...
-
-# Eliminar el archivo HDF5 después de su uso (opcional)
-os.remove(nombre_archivo_local)
+st.success("Ruta completa del archivo:", os.path.abspath(ruta_archivo))

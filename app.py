8815915dd1ff0edd18f7ec2a97523d8910924abd
Gabriela Quinde <50193708+GabbyLiz@users.noreply.github.com>
2024-03-01 17:47:06 -0500

    Update app.py
    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from tensorflow.keras.models import load_model
 import gdown
+import os
 
 st.title('Mi Aplicación Streamlit con Modelo Preentrenado')
 
@@ -10,9 +11,23 @@ enlace_google_drive = 'https://drive.google.com/uc?id=1uiJR1cD2W1cNVpqG77Th6XHhW
 # Nombre del archivo HDF5 local (no es necesario descargarlo localmente)
 nombre_archivo_local = 'modelo.hdf5'
 
-# Descargar el modelo directamente desde el enlace de Google Drive
+# Comprobar si el modelo ya está descargado
+if not os.path.exists(nombre_archivo_local):
+    st.info('Descargando el modelo...')
+    try:
+        gdown.download(enlace_google_drive, nombre_archivo_local, quiet=False)
+        st.success('Modelo descargado exitosamente.')
+    except Exception as e:
+        st.error(f'Error al descargar el modelo: {str(e)}')
+else:
+    st.info('El modelo ya está descargado.')
+
+# Comprobar el contenido del directorio actual
+contenido_directorio = os.listdir()
+st.write('Contenido del directorio actual:', contenido_directorio)
+
+# Intentar cargar el modelo
 try:
-    gdown.download(enlace_google_drive, nombre_archivo_local, quiet=False)
     modelo_cargado = load_model(nombre_archivo_local)
     st.success('Modelo cargado exitosamente.')
 except Exception as e:
